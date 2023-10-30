d476488fc5ec8fa3e7e19ccb845beeb72543fb2f
Ann <annclardner@gmail.com>
2023-10-30 16:38:43 +0000

    Initial commit

---
+++ src/main/java/com/example/demo/Ct5171DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ct5171DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ct5171DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Ct5171DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ct5171DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
